7636e5d3eb8f1fa9aae380b98f72175853f0e960
dany.bony <dany.bony@gmail.com>
2008-02-27 18:13:55 +0000

    Creata la classe OperazioneInMemoria nel package logic.gestioneMemoria

---
+++ src/logic/gestioneMemoria/OperazioneInMemoria.java
@@ -0,0 +1,19 @@
+
+package logic.gestioneMemoria;
+
+/*
+ * Azienda: Stylosoft
+ * Nome file: Processore.java
+ * Package: logic
+ * Autore: Daniele Bonaldo
+ * Data: 27/02/2008
+ * Versione: 1.2
+ * Licenza: open-source
+ * Registro delle modifiche: *  
+ *  - v.1.2 (27/02/2008): Modifica del metodo calcolaFault.
+ *  - v.1.1 (26/02/2008): Aggiunta la descrizione dei parametri nella javadoc.
+ *  - v.1.0 (26/02/2008): Creazione e scrittura documentazione.
+ */
+public class OperazioneInMemoria {
+
+}
