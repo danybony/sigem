89124522308d7687f0edaabc6c978194e3d60bdf
Jordywild <Jordywild@b74f90e9-8743-0410-9cba-1b647e1d917f>
2008-02-29 00:21:34 +0000

    Interfaccia SigemV2

---
+++ src/gui/dialog/ModelloProcessi.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui.dialog;
+import javax.swing.JComboBox;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.DefaultCellEditor;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.DefaultTableCellRenderer;
+/**
+ *
+ * @author Jordy
+ */
+public class ModelloProcessi extends AbstractTableModel {
+        private String[] nomiColonna=new String[0];
+        
+        private Object[][] contenutiRighe=new Object[0][0];
+        
+        public ModelloProcessi(int nRighe) {
+            nomiColonna = new String[] {"Nome", "Tempo di arrivo", "Tempo di esecuzione" };
+            contenutiRighe=new Object[nRighe][3];
+            for (int i=0; i<nRighe; i++) {
+                contenutiRighe[i][0]=new String("P"+(i+1));
+                contenutiRighe[i][1]=new Integer(0);
+                contenutiRighe[i][2]=new Integer(1);
+            }
+            fireTableStructureChanged();
+        }
+
+        public ModelloProcessi(Object[][] table, int nColonne) {
+            nomiColonna = new String[] {"Nome", "Tempo di arrivo", "Tempo di esecuzione","Associazione" };
+            contenutiRighe=new Object[table.length][nColonne];
+            
+            for (int i=0; i<table.length; i++) {
+                contenutiRighe[i][0]=new String((String) table[i][0]);
+                contenutiRighe[i][1]=new Integer((Integer) table[i][1]);
+                contenutiRighe[i][2]=new Integer((Integer) table[i][2]);
+            }
+            fireTableStructureChanged();
+        }
+        public int getColumnCount() {
+            return nomiColonna.length;
+        }
+
+        public int getRowCount() {
+            return contenutiRighe.length;
+        }
+
+        public String getColumnName(int col) {
+            return nomiColonna[col];
+        }
+
+        public Object getValueAt(int row, int col) {
+            return contenutiRighe[row][col];
+        }
+
+        /*
+         * JTable uses this method to determine the default renderer/
+         * editor for each cell.  If we didn't implement this method,
+         * then the last column would contain text ("true"/"false"),
+         * rather than a check box.
+         */
+        public Class getColumnClass(int c) {
+            return getValueAt(0, c).getClass();
+        }
+
+        /*
+         * Don't need to implement this method unless your table's
+         * editable.
+         */
+        public boolean isCellEditable(int row, int col) {
+            //Note that the contenutiRighe/cell address is constant,
+            //no matter where the cell appears onscreen.
+            if (nomiColonna.length < 4) 
+                return true;
+            else {
+               if (col < 3)
+                   return false;
+               else
+                return true;
+            }
+        }
+
+        /*
+         * Don't need to implement this method unless your table's
+         * contenutiRighe can change.
+         */
+        public void setValueAt(Object value, int row, int col) {
+            contenutiRighe[row][col] = value;
+            fireTableCellUpdated(row, col);
+        }
+    }
