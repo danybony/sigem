e500acdae9ef01d6dd044836a792dd66b1297e13
luca86r <luca86r@b74f90e9-8743-0410-9cba-1b647e1d917f>
2008-02-29 17:02:16 +0000

    prima di togliere l'iteratore

--- src/logic/caricamento/GestioneFile.java
+++ src/logic/caricamento/GestioneFile.java
@@ -2,70 +2,48 @@ package logic.caricamento;
  
 import logic.parametri.ConfigurazioneIniziale; 
 
-// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-// #[regen=yes,id=DCE.E8207814-BAF5-432A-B065-C89144BE1829]
-// </editor-fold> 
+
 public class GestioneFile {
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.A8C6CBDE-B363-09EE-5637-E0DEB7D441C5]
-    // </editor-fold> 
+
     private String percosoFileConfigurazione;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.9A516B6B-604A-A3E6-8309-3DC1A3F6220D]
-    // </editor-fold> 
+
     private String percosoFileLog;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.1A3A5949-D07B-7F5B-BC7D-1D44690E88AD]
-    // </editor-fold> 
+
     private ConfigurazioneIniziale mConfigurazioneIniziale;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.F85B73D1-01D8-FF74-C800-80A414A697A2]
-    // </editor-fold> 
+    
     public String getPercorsoFileConfigurazione () {
         return null;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.6A23AAE2-0A68-D883-0011-712301DAB665]
-    // </editor-fold> 
+
     public void setPercorsoFileConfigurazione (String val) {
         
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.C2EBD2DB-531A-B734-47A2-4D41D76192BF]
-    // </editor-fold> 
+
     public String getPercorsoFileLog () {
         return null;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.4030D646-2AB6-A252-4790-5E0EF6A30084]
-    // </editor-fold> 
+
     public void setPercorsoFileLog (String val) {
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.DDB7AC84-5B87-115F-77A9-E71B0BC21321]
-    // </editor-fold> 
+
     public boolean salvaFileConfigurazione (ConfigurazioneIniziale val) {
         return true;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.8304337F-401A-EE7E-D6C0-4670FDBD2645]
-    // </editor-fold> 
+ 
     public boolean caricaFileConfigurazione () {
         return true;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.32C70857-65B4-A588-BD88-79A1EAB03C15]
-    // </editor-fold> 
+
     public boolean aggiungiAlLog (String val) {
         return true;
     }
