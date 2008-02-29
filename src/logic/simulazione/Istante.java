4dbfbb881b18e74bdda38758db9203c78e75aee9
luca86r <luca86r@b74f90e9-8743-0410-9cba-1b647e1d917f>
2008-02-29 15:52:16 +0000

    prima di togliere l'iteratore

--- src/logic/simulazione/Istante.java
+++ src/logic/simulazione/Istante.java
@@ -5,26 +5,143 @@ import java.util.LinkedList;
 import logic.gestioneMemoria.OperazioneInMemoria;
 import logic.schedulazione.PCB;
 
-
+/**
+ * Classe che memorizza le caratteristiche della simulazione per ogni unita'
+ * di tempo.<br>
+ * Ogni istanza di questa classe rappresenta in modo differenziale le 
+ * caratteristiche che la simulazione assume al tempo t.<br>
+ * Tale istanti potranno essere utilizzati dalla GUI tramite la classe
+ * Player per visualizzare l'avenzamento della simulazione.
+ * Dato che i dati sono memorizzati in modo differenziale, la memoria utilizzata
+ * dalla simulazione risulta ridotta.
+ */
 public class Istante {
     
-    private PCB processoInEsecuzione;
-    private PCB processoPrecedenteTerminato;
-    private int numeroFault;
+    /**
+     * PCB del processo in esecuzione nell'unita' di tempo rappresentata
+     * dall'istanza di Istante.
+     */
+    private PCB processoInEsecuzione = null;
+    
+    /**
+     * Eventuale PCB del processo terminato all'istante t.
+     */
+    private PCB processoPrecedenteTerminato = null;
+    
+    /**
+     * Segnala l'arrivo di uno o più processi nella coda dei pronti.
+     */
+    private boolean nuovoProcesso = false;
+    
+    /**
+     * Numero di fault avvenuti in memoria nell'istante t.
+     */
+    private int numeroFault = 0;
+    
+    /**
+     * Lista dei cambiamenti avvenuti in memoria: aggiunta o rimozione di 
+     * pagine/segmenti.
+     */
     private LinkedList<OperazioneInMemoria> cambiamentiInMemoria;
+    
+    /**
+     * Segnala l'eventuale riempimento della memoria centrale RAM.
+     */
+    private boolean full_RAM = false;
+    
+    /**
+     * Segnala l'eventuale riempimento dell'area di Swap.
+     */
+    private boolean full_Swap = false;
     
     
     /**
+     * Costruttore della classe.<br>
+     * Provvede all'inizializzazione di tutti i campi dato della classe.
      * 
      * @param inEsecuzione
+     *      PCB del processo in esecuzione all'istante t
      * @param terminato
+     *      PCB dell'eventuale processo terminato all'istante t
+     * @param nuovoProcesso
+     *      booleano che dichiara l'arrivo di uno o piu' nuovi processi nella
+     *      coda dei pronti all'istante t
      * @param fault
+     *      numero di fault avvenuti in memoria all'istante t
      * @param memoria
+     *      operazioni effettuate in memoria all'istante t (rimozione e/o aggiunta
+     *      di pagine o segmenti)
+     * @param full_RAM
+     *      dichiara il riempimento della memoria centrale RAM all'istante t
+     * @param full_Swap
+     *      dichiara il riempimento dell'area di Swap all'istante t
      */
-    public Istante(PCB inEsecuzione, PCB terminato, int fault, LinkedList<OperazioneInMemoria> memoria){
+    public Istante(
+            PCB inEsecuzione,
+            PCB terminato,
+            boolean nuovoProcesso,
+            int fault,
+            LinkedList<OperazioneInMemoria> memoria,
+            boolean full_RAM,
+            boolean full_Swap
+            ){
         this.processoInEsecuzione = inEsecuzione;
         this.processoPrecedenteTerminato = terminato;
+        this.nuovoProcesso = nuovoProcesso;
         this.numeroFault = fault;
         this.cambiamentiInMemoria = memoria;
+        this.full_RAM = full_RAM;
+        this.full_Swap = full_Swap;
+    }
+    
+    /**
+     * Ritorna il PCB del processo in esecuzione all'istante t.
+     */
+    public PCB getProcessoInEsecuzione(){
+        return this.processoInEsecuzione;
+    }
+    
+    /**
+     * Ritorna il PCB del processo terminato all'istante t. Nel caso venga 
+     * ritornato un riferimento nullo, nessun processo e' stato terminato.
+     */
+    public PCB getProcessoPrecedenteTerminato(){
+        return this.processoPrecedenteTerminato;
+    }
+    
+    /**
+     * Ritorna un booleano che indica l'eventuale arrivo di nuovi processi nella
+     * coda dei pronti.
+     */
+    public boolean getNuovoProcesso(){
+        return this.nuovoProcesso;
+    }
+    
+    /**
+     * Ritorna il numero di fault avvenuti in memori all'istante t.
+     */
+    public int getFault(){
+        return this.numeroFault;
+    }
+    
+    /**
+     * Ritorna una lista che descrive le operazioni avvenute in memoria.
+     */
+    public LinkedList<OperazioneInMemoria> getCambiamentiInMemoria(){
+        return this.cambiamentiInMemoria;
+    }
+    
+    /**
+     * Ritorna un booleano che indica l'eventuale riempimento della RAM.
+     */
+    public boolean getFull_RAM(){
+        return this.full_RAM;
+    }
+    
+    /**
+     * Ritorna un booleano che indica l'eventuale riempimento dell'area di Swap.
+     */
+    public boolean getFull_Swap(){
+        return this.full_Swap;
     }
 }
