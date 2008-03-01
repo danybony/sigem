8411c1e90c184616157efff2ee7f0b295e577837
dcompagn <dcompagn@b74f90e9-8743-0410-9cba-1b647e1d917f>
2008-03-01 10:18:01 +0000

    (Clock) Versione 1.0

---
+++ src/logic/gestioneMemoria/C.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package logic.gestioneMemoria;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Davide
+ */
+public class C implements IRimpiazzo {
+    private int Lancetta=0;
+    
+    private class Dati {
+      private boolean R;
+      Dati() { R=false; }
+    }
+    
+    private Vector<Dati> Tabella=new Vector<Dati>();
+    
+    
+    C( int dim ) {
+        for( int i=0; i<dim; i++ )
+            Tabella.add( new Dati() );
+    }
+    
+    public void InserisciEntry( int Posizione, int UT, boolean M ) { 
+        Tabella.elementAt(Posizione).R=true;
+    }
+    
+    public void LiberaEntry( int Posizione ) { 
+        Tabella.elementAt(Posizione).R=false;
+    }
+    
+    public int SelezionaEntry() {
+        boolean trovata=false;
+        int dim=Tabella.size();        
+        while( !trovata ) {
+            if ( Tabella.elementAt(Lancetta).R==true ) {
+                Tabella.elementAt(Lancetta).R=false;
+                Lancetta=(Lancetta+1)%dim;
+            }
+            else trovata=true;
+        }
+        int temp=Lancetta;
+        Lancetta=(Lancetta+1)%dim;
+        return temp;
+    }
+    
+    public void AggiornaEntry( int Posizione, boolean M ) {}
+    public void AggiornaEntries( ) {}
+}
