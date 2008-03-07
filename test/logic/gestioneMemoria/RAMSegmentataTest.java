a80f71eb0e6e8b31c72b196cac6bdf16e3c4694b
alzatxxx <alzatxxx@gmail.com>
2008-03-07 16:18:55 +0000

    Test per la RAM segmentata completato

--- test/logic/gestioneMemoria/RAMSegmentataTest.java
+++ test/logic/gestioneMemoria/RAMSegmentataTest.java
@@ -45,93 +45,205 @@ public class RAMSegmentataTest extends TestCase {
     /**
      * Test of aggiungi method, of class RAMSegmentata.
      */
-    public void testAggiungi() {
-        System.out.println("aggiungi");
+    public void testAggiungi1() {
+        //Caso 1: RAM vuota
+        System.out.println("aggiungi Caso 1: RAM vuota");
         FrameMemoria seg = new Segmento("prova",1024,2);
         FrameMemoria spazio = memoria.getSpazioMaggiore();
-        RAMSegmentata instance = null;
+        RAMSegmentata instance = memoria;
         instance.aggiungi(seg, spazio);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
+    }
+    
+    public void testAggiungi2() throws Exception {
+        memoria.aggiungi(s1,memoria.getSpazioMaggiore());
+        //Caso 2: RAM che contiene già un segmento
+        System.out.println("aggiungi Caso 2: RAM che contiene già un segmento");
+        FrameMemoria seg = new Pagina("prova",1024,0);
+        RAMSegmentata instance = memoria;
+        instance.aggiungi(seg,memoria.getSpazioMaggiore());
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
     }
 
     /**
      * Test of rimuovi method, of class RAMSegmentata.
      */
-    public void testRimuovi() {
-        System.out.println("rimuovi");
+    public void testRimuovi1() {
+        //Caso 1: RAM vuota
+        System.out.println("rimuovi Caso 1: RAM vuota");
         FrameMemoria seg = s1;
+        RAMSegmentata instance = memoria;
+        boolean expResult = false;
+        boolean result = instance.rimuovi(seg);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
+    }
+    
+    public void testRimuovi2() {
+        memoria.aggiungi(s2,memoria.getSpazioMaggiore());
+        //Caso 2: La RAM contiene il segmento
+        System.out.println("rimuovi Caso 2: La RAM contiene il segmento");
+        FrameMemoria seg = s2;
         RAMSegmentata instance = memoria;
         boolean expResult = true;
         boolean result = instance.rimuovi(seg);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
     }
 
     /**
      * Test of liberaMemoria method, of class RAMSegmentata.
      */
-    public void testLiberaMemoria() {
-        System.out.println("liberaMemoria");
+    public void testLiberaMemoria1() {
+        //Caso 1: RAM non contiene nessun segmento riferito dal processo
+        System.out.println("liberaMemoria Caso 1: RAM non contiene nessun segmento riferito dal processo");
         int idProcesso = 0;
-        RAMSegmentata instance = null;
+        RAMSegmentata instance = memoria;
         instance.liberaMemoria(idProcesso);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
+    }
+    
+    public void testLiberaMemoria2() {
+        memoria.aggiungi(s3,memoria.getSpazioMaggiore());
+        //Caso 2: RAM contiene il segmento riferito dal processo
+        System.out.println("liberaMemoria Caso 2: RAM contiene il segmento riferito dal processo");
+        int idProcesso = 2;
+        RAMSegmentata instance = memoria;
+        instance.liberaMemoria(idProcesso);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
     }
 
     /**
      * Test of getFrameLiberi method, of class RAMSegmentata.
      */
-    public void testGetFrameLiberi() {
-        System.out.println("getFrameLiberi");
-        RAMSegmentata instance = null;
-        Vector<FrameMemoria> expResult = null;
-        Vector<FrameMemoria> result = instance.getFrameLiberi();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testGetFrameLiberi1() {
+        //Caso 1: RAM vuota
+        System.out.println("getFrameLiberi Caso 1: RAM vuota");
+        if(memoria.getFrameLiberi().get(0).getDimensione()!=4096)
+            fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
+    }
+    
+    public void testGetFrameLiberi2() {
+        memoria.aggiungi(s1,memoria.getSpazioMaggiore());
+        memoria.aggiungi(s1,memoria.getSpazioMaggiore());
+        memoria.aggiungi(s2,memoria.getSpazioMaggiore());
+        memoria.aggiungi(s3,memoria.getSpazioMaggiore());
+        //Caso 2: RAM piena
+        System.out.println("getFrameLiberi Caso 2: RAM piena");
+        if(!memoria.getFrameLiberi().isEmpty())
+            fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
     }
 
     /**
      * Test of getFrameOccupati method, of class RAMSegmentata.
      */
-    public void testGetFrameOccupati() {
-        System.out.println("getFrameOccupati");
-        RAMSegmentata instance = null;
-        Vector<FrameMemoria> expResult = null;
-        Vector<FrameMemoria> result = instance.getFrameOccupati();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testGetFrameOccupati1() {
+        //Caso 1: RAM non contiene segmenti
+        System.out.println("getFrameOccupati Caso 1: RAM non contiene segmenti");
+        if(!memoria.getFrameOccupati().isEmpty())
+            fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
+    }
+    
+    public void testGetFrameOccupati2() {
+        memoria.aggiungi(s1, memoria.getSpazioMaggiore());
+        //Caso 2: RAM contiene un segmento
+        System.out.println("getFrameOccupati Caso 2: RAM contiene un segmento");
+        if(memoria.getFrameOccupati().size()!=1)
+            fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
     }
 
     /**
      * Test of getSpazioMaggiore method, of class RAMSegmentata.
      */
-    public void testGetSpazioMaggiore() {
-        System.out.println("getSpazioMaggiore");
-        RAMSegmentata instance = null;
-        FrameMemoria expResult = null;
-        FrameMemoria result = instance.getSpazioMaggiore();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testGetSpazioMaggiore1() {
+        //Caso 1: RAM vuota
+        System.out.println("getSpazioMaggiore Caso 1: RAM vuota");
+        if(memoria.getSpazioMaggiore().getDimensione()!=4096)
+            fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
+    }
+    
+    public void testGetSpazioMaggiore2() {
+        memoria.aggiungi(s1,memoria.getSpazioMaggiore());
+        memoria.aggiungi(s1,memoria.getSpazioMaggiore());
+        memoria.aggiungi(s2,memoria.getSpazioMaggiore());
+        memoria.aggiungi(s3,memoria.getSpazioMaggiore());
+        //Caso 2: RAM piena
+        System.out.println("getSpazioMaggiore Caso 2: RAM piena");
+        if(memoria.getSpazioMaggiore().getDimensione()!=0)
+            fail("The test case is a prototype.");
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
     }
 
     /**
      * Test of cerca method, of class RAMSegmentata.
      */
-    public void testCerca() {
-        System.out.println("cerca");
-        FrameMemoria seg = null;
-        RAMSegmentata instance = null;
+    public void testCerca1() {
+        //Caso 1: RAM vuota
+        System.out.println("cerca Caso 1: RAM vuota");
+        FrameMemoria seg = s1;
+        RAMSegmentata instance = memoria;
         boolean expResult = false;
         boolean result = instance.cerca(seg);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.")
+    }
+    
+    public void testCerca2() {
+        memoria.aggiungi(s1, memoria.getSpazioMaggiore());
+        //Caso 2: RAM contiene il segmento cercato
+        System.out.println("cerca Caso 2: RAM contiene il segmento cercato");
+        FrameMemoria seg = s1;
+        RAMSegmentata instance = memoria;
+        boolean expResult = true;
+        boolean result = instance.cerca(seg);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.")
+        
+        //Resetto la memoria
+        memoria=new RAMSegmentata(conf);
     }
 
 }
