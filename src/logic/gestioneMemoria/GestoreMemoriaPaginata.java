f8fddfa9603d0f7cfad7c42236f1fb1062693d8e
dcompagn <dcompagn@b74f90e9-8743-0410-9cba-1b647e1d917f>
2008-03-07 19:06:05 +0000

    Aggiunto controllo inserimento numero maggiore di pagine rispetto a quelle disponibili

--- src/logic/gestioneMemoria/GestoreMemoriaPaginata.java
+++ src/logic/gestioneMemoria/GestoreMemoriaPaginata.java
@@ -2,6 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  * Aggiunto controllo rimozione pagina non esistente
+ * Aggiunto controllo inserimento numero maggiore di pagine rispetto a quelle disponibili
  */
 
 package logic.gestioneMemoria;
